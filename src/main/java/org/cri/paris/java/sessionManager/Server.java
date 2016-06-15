2b1e2f670b78308949e25a846362c9adc114a834
Arthur Besnard <arthur.bd@wanadoo.fr>
2016-06-15 19:32:01 +0200

    Added config file

--- src/main/java/org/cri/paris/java/sessionManager/Server.java
+++ src/main/java/org/cri/paris/java/sessionManager/Server.java
@@ -2,6 +2,13 @@
 package org.cri.paris.java.sessionManager;
 
 import io.vertx.core.Vertx;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.cri.configurator.Config;
+import org.cri.configurator.Configs;
+
 
 /**
  * Main class used to launch the VertX server
@@ -14,24 +21,32 @@ public class Server {
      * @param args : command line arguments
      */
     public static void main(String[] args) {
-        //TODO use config management
-        final String HOST = "localhost";
-        final int PORT = 5432;
-        final int MAX_POOL_SIZE = 256;
-        final String USERNAME = "manager";
-        final String PASSWORD = "slkdfj315746";
-        final String DATABASE = "player";
-        final String CLIENT_ID = "118231460924-7cmfpiefralb5ij4uv7kv6okcolhrc3k.apps.googleusercontent.com";
-        Vertx vertx = Vertx.vertx();
-        SessionControler service = 
-                new SessionControler(SessionModel.getSessionManager(vertx, 
-                    HOST, 
-                    PORT, 
-                    MAX_POOL_SIZE, 
-                    USERNAME, 
-                    PASSWORD, 
-                    DATABASE), 
-                new IdentityManager(CLIENT_ID));
-        vertx.deployVerticle(service);
+        
+        try {
+            Config<String, String> conf = Configs.getSimpleConfig(Paths.get("/etc/redlogin/redlogin.conf"),
+                    "host",
+                    "port",
+                    "max_pool",
+                    "db_username",
+                    "db_password",
+                    "db_name",
+                    "google_client_id");
+            
+            Vertx vertx = Vertx.vertx();
+            SessionControler service =
+                    new SessionControler(SessionModel.getSessionManager(vertx,
+                            conf.get("host"),
+                            Integer.parseInt(conf.get("port")),
+                            Integer.parseInt(conf.get("max_pool")),
+                            conf.get("db_username"),
+                            conf.get("db_password"),
+                            conf.get("db_name")),
+                            new IdentityManager(conf.get("google_client_id")));
+            vertx.deployVerticle(service);
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, "Unable to open configuration file : ", ex);
+        } catch (IllegalStateException ise){
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, "Error parsing configuration : ", ise);
+        }
     }
 }
